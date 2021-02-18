0193ea056c7fdab08dc8578dedaeb44261087c6b
Jeff <tibame201020@gmail.com>
2021-02-18 14:57:14 +0800

    Add Math return SUM

--- GitDemo/src/der/Hello.java
+++ GitDemo/src/der/Hello.java
@@ -6,6 +6,15 @@ public class Hello {
 		// TODO Auto-generated method stub
 //Ver.1
 		System.out.println("Ver.1");
+		System.out.println();
+		Math cMath = new Math();
+		System.out.println("cMath.add(5, 6)="+ cMath.add(5, 6));
 	}
 
+}
+
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
